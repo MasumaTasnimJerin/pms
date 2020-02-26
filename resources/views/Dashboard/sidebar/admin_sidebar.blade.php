05775f03416e6da4e434c1056c8e285824259d1a
Masuma Tasnim Jerin <masumatasnim41@gmail.com>
2020-02-27 00:34:27 +0600

    2nd

---
+++ resources/views/Dashboard/sidebar/admin_sidebar.blade.php
@@ -0,0 +1,152 @@
+<div class="nav-left-sidebar sidebar-dark">
+            <div class="menu-list">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                     data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav flex-column">
+                            <li class="nav-divider">
+                                Dashboard
+                            </li>
+                            <li class="nav-item ">
+                    <!-- Users  -->
+                                <a class="nav-link active" href="#" data-toggle="collapse"
+                                aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1">
+                                <i class="fa fa-fw fa-user-circle"></i>Users <span class="badge badge-success">6</span></a>
+
+                                <div id="submenu-1" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="dashboard-finance.html">User List</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="dashboard-sales.html">Add User Role</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="dashboard-sales.html">Role List</a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- Medicine  -->
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" data-toggle="collapse"
+                                aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2">
+                                <i class="fa fa-fw fa-rocket"></i>Medicine</a>
+                                <div id="submenu-2" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/general.html">Medicine List</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/carousel.html">Add Medicine</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/general.html">Medicine Catagory</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/carousel.html">Add Catagory</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- Purchase  -->
+
+
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                                data-target="#submenu-3" aria-controls="submenu-3">
+                                <i class="fab fa-fw fa-wpforms"></i>Purchase </a>
+                                <div id="submenu-3" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/form-elements.html">Add Purchase</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/form-validation.html">Manage Purchase</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- Stock -->
+
+                            <li class="nav-item">
+                              <a class="nav-link" href=""><i class="fas fa-fw fa-users"></i>
+                                <span>Stock</span></a>
+                            </li>
+
+                            <!-- POS -->
+                            <li class="nav-item">
+                              <a class="nav-link" href=""><i class="fas fa-fw fa-users"></i>
+                                <span>POS</span></a>
+                            </li>
+
+                            <!-- All Sales -->
+                            <li class="nav-item">
+                              <a class="nav-link" href=""><i class="fas fa-fw fa-users"></i>
+                                <span>All Sales</span></a>
+                            </li>
+
+                            <!-- Suppliers -->
+
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                                data-target="#submenu-4" aria-controls="submenu-4">
+                                <i class="fab fa-fw fa-wpforms"></i>Suppliers </a>
+                                <div id="submenu-4" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/form-elements.html">Add Supplier</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/form-validation.html">Supplier Lists</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+
+                            <!-- Reports -->
+                            <li class="nav-item ">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false"
+                                data-target="#submenu-5" aria-controls="submenu-5">
+                                <i class="fab fa-fw fa-wpforms"></i>Reports </a>
+                                <div id="submenu-5" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/form-elements.html">Sales Report</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="pages/form-validation.html">Purchase Report</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+
+
+
+
+
+                          </ul>
+                      </div>
+                  </nav>
+              </div>
+              </div>            </div>
+                            </li>
+                                    </ul>
+                                  </div>
+                              </li>
+                          </ul>
+                      </div>
+                  </nav>
+              </div>
+          </div>
